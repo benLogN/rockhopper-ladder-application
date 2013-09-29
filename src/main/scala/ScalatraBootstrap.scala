151614a337ca8e4274fe6ce53f67eca276db0acf
root <root@devboxa.novalocal>
2013-09-29 01:01:21 +0000

    Set up for actors

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,21 @@
+import _root_.akka.actor.{ActorSystem, Props}
 import es.chickade.rockhopper._
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
+
+  val system = ActorSystem()
+//  val myActor = system.actorOf(Props[MyActor])
+
   override def init(context: ServletContext) {
-    context.mount(new RockhopperServlet, "/*")
+    context.mount(new RockhopperServlet(system), "/*")
+//    context.mount(new PageRetriever(system), "/*")
+//    context.mount(new MyActorApp(system, myActor), "/actors/*")
   }
+
+  override def destroy(context:ServletContext) {
+    system.shutdown()
+  }
+
 }
--- src/main/scala/es/chickade/rockhopper/RockhopperServlet.scala
+++ src/main/scala/es/chickade/rockhopper/RockhopperServlet.scala
@@ -2,14 +2,29 @@ package es.chickade.rockhopper
 
 import org.scalatra._
 import scalate.ScalateSupport
+import scala.concurrent.{Await, Future, ExecutionContext}
+import _root_.akka.actor.ActorSystem
+import org.scalatra.FutureSupport
+import scala.xml.Node
 
-class RockhopperServlet extends RockhopperLadderApplicationStack {
+class RockhopperServlet(system: ActorSystem) extends RockhopperLadderApplicationStack with FutureSupport{
+
+  protected implicit def executor: ExecutionContext = system.dispatcher
 
   get("/") {
-    <p>You sent me nothing!</p>
+    new AsyncResult { val is =
+      Future{
+        <p>You sent me nothing</p>
+      }
+    }
   }
+
   get("/:param") {
-    <p>You sent me {params("param")}</p>
+    new AsyncResult { val is =
+      Future{
+        <p>You sent me {params("param")}</p>
+      }
+    }
   }
   
 }
